2e96153d18ac2cb9c2e0d4587d4f446193ece8a9
jonyShev <jony.a.shev@gmail.com>
2025-06-26 16:07:51 +0200

    Создал новый метод updateCartFromMain в ItemController

--- src/main/java/com/jonyshev/intershop/controller/ItemController.java
+++ src/main/java/com/jonyshev/intershop/controller/ItemController.java
@@ -1,12 +1,17 @@
 package com.jonyshev.intershop.controller;
 
+import com.jonyshev.intershop.model.CartAction;
 import com.jonyshev.intershop.model.Item;
 import com.jonyshev.intershop.model.PagingInfo;
+import com.jonyshev.intershop.service.CartService;
 import com.jonyshev.intershop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -15,6 +20,8 @@ import java.util.List;
 public class ItemController {
 
     private final ItemService itemService;
+
+    private final CartService cartService;
 
     @GetMapping("/")
     public String redirectToMainItems() {
@@ -36,4 +43,13 @@ public class ItemController {
         return "main";
     }
 
+    @PostMapping("/main/items/{id}")
+    public String updateCartFromMain(@PathVariable Long id, @RequestParam CartAction action) {
+        switch (action) {
+            case PLUS -> cartService.addItem(id);
+            case MINUS -> cartService.decreaseItem(id);
+            case DELETE -> cartService.deleteItem(id);
+        }
+        return "redirect:/main/items";
+    }
 }
