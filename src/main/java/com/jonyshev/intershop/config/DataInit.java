e13c6d73cb555516698b3668337c8a640d56a035
jonyShev <jony.a.shev@gmail.com>
2025-06-29 22:55:50 +0200

    Добавил дополнительные  товары

--- src/main/java/com/jonyshev/intershop/config/DataInit.java
+++ src/main/java/com/jonyshev/intershop/config/DataInit.java
@@ -17,47 +17,21 @@ public class DataInit {
         return args -> {
             if (itemRepository.count() == 0) {
                 itemRepository.saveAll(List.of(
-                        Item.builder()
-                                .title("Пицца")
-                                .description("Вкусная пицца с сыром")
-                                .imgPath("/images/pizza.png")
-                                .price(new BigDecimal("12.99"))
-                                .build(),
-
-                        Item.builder()
-                                .title("Бургер")
-                                .description("Сочный бургер с говядиной")
-                                .imgPath("/images/burger.png")
-                                .price(new BigDecimal("8.99"))
-                                .build(),
-
-                        Item.builder()
-                                .title("Суши")
-                                .description("Набор роллов")
-                                .imgPath("/images/sushi.png")
-                                .price(new BigDecimal("15.99"))
-                                .build(),
-
-                        Item.builder()
-                                .title("Паста")
-                                .description("Паста карбонара")
-                                .imgPath("/images/pasta.png")
-                                .price(new BigDecimal("10.99"))
-                                .build(),
-
-                        Item.builder()
-                                .title("Стейк")
-                                .description("Говяжий стейк")
-                                .imgPath("/images/steak.png")
-                                .price(new BigDecimal("18.50"))
-                                .build(),
-
-                        Item.builder()
-                                .title("Салат")
-                                .description("Овощной салат")
-                                .imgPath("/images/salad.png")
-                                .price(new BigDecimal("6.75"))
-                                .build()
+                        Item.builder().title("Пицца").description("Вкусная пицца с сыром").imgPath("/images/pizza.png").price(BigDecimal.valueOf(550)).build(),
+                        Item.builder().title("Бургер").description("Сочный бургер с говядиной").imgPath("/images/burger.png").price(BigDecimal.valueOf(350)).build(),
+                        Item.builder().title("Суши").description("Набор роллов").imgPath("/images/sushi.png").price(BigDecimal.valueOf(750)).build(),
+                        Item.builder().title("Паста").description("Паста карбонара").imgPath("/images/pasta.png").price(BigDecimal.valueOf(450)).build(),
+                        Item.builder().title("Стейк").description("Говяжий стейк").imgPath("/images/steak.png").price(BigDecimal.valueOf(1200)).build(),
+                        Item.builder().title("Салат").description("Овощной салат").imgPath("/images/salad.png").price(BigDecimal.valueOf(250)).build(),
+                        Item.builder().title("Шаурма").description("Сочная шаурма с курицей").imgPath("/images/shaurma.png").price(BigDecimal.valueOf(300)).build(),
+                        Item.builder().title("Картошка фри").description("Хрустящая картошка фри").imgPath("/images/fries.png").price(BigDecimal.valueOf(150)).build(),
+                        Item.builder().title("Хот-дог").description("Классический хот-дог с сосиской").imgPath("/images/hotdog.png").price(BigDecimal.valueOf(200)).build(),
+                        Item.builder().title("Лапша Wok").description("Лапша Wok с овощами и курицей").imgPath("/images/wok.png").price(BigDecimal.valueOf(400)).build(),
+                        Item.builder().title("Том Ям").description("Острый тайский суп с креветками").imgPath("/images/tomyam.png").price(BigDecimal.valueOf(600)).build(),
+                        Item.builder().title("Пельмени").description("Русские пельмени со сметаной").imgPath("/images/pelmeni.png").price(BigDecimal.valueOf(350)).build(),
+                        Item.builder().title("Куриные крылышки").description("Острые куриные крылышки").imgPath("/images/wings.png").price(BigDecimal.valueOf(450)).build(),
+                        Item.builder().title("Чизкейк").description("Классический ванильный чизкейк").imgPath("/images/cheesecake.png").price(BigDecimal.valueOf(300)).build(),
+                        Item.builder().title("Мороженое").description("Пломбир с шоколадным топпингом").imgPath("/images/icecream.png").price(BigDecimal.valueOf(180)).build()
                 ));
             }
         };
