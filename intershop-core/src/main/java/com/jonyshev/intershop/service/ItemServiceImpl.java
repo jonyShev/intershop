a1dcf5aa2291f37d83157912dca2eff321299e95
jonyShev <jony.a.shev@gmail.com>
2025-08-06 15:35:02 +0200

    Добавил кэширование в ItemServiceImpl

--- intershop-core/src/main/java/com/jonyshev/intershop/service/ItemServiceImpl.java
+++ intershop-core/src/main/java/com/jonyshev/intershop/service/ItemServiceImpl.java
@@ -4,11 +4,13 @@ import com.jonyshev.intershop.dto.ItemDto;
 import com.jonyshev.intershop.model.Item;
 import com.jonyshev.intershop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.WebSession;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +20,7 @@ public class ItemServiceImpl implements ItemService {
 
     private final ItemRepository itemRepository;
     private final CartService cartService;
+    private final ReactiveRedisTemplate<String, ItemDto> redisTemplate;
 
     @Override
     public Flux<Item> getAllItems(String search, String sort, int pageSize, int pageNumber) {
@@ -49,15 +52,24 @@ public class ItemServiceImpl implements ItemService {
 
     @Override
     public Mono<ItemDto> getItemDtoById(Long id, WebSession session) {
-        return itemRepository.findById(id)
-                .switchIfEmpty(Mono.error(new IllegalArgumentException("Item not found" + id)))
-                .flatMap(item -> mapToDto(item, session));
+        String key = "item:" + id;
+        return redisTemplate.opsForValue().get(key)
+                .switchIfEmpty(
+                        itemRepository.findById(id)
+                                .switchIfEmpty(Mono.error(new IllegalArgumentException("Item not found" + id)))
+                                .flatMap(item -> mapToDto(item, session))
+                                .flatMap(dto -> redisTemplate
+                                        .opsForValue()
+                                        .set(key, dto, Duration.ofMinutes(3))
+                                        .thenReturn(dto)
+                                )
+                );
     }
 
     @Override
     public Mono<List<List<ItemDto>>> getItemChunks(String search, String sort, int pageSize, int pageNumber, WebSession session) {
         return getAllItems(search, sort, pageSize, pageNumber)
-                .flatMap(item -> mapToDto(item, session))
+                .flatMap(item -> getItemDtoById(item.getId(), session))
                 .collectList()
                 .map(itemDtos -> chunkItems(itemDtos, 3));
     }
