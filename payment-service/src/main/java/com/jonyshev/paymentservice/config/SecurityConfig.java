886ef8105e265c5307a44ec8527300825970b166
jonyShev <jony.a.shev@gmail.com>
2025-08-29 14:40:30 +0200

    Добавил временный SecurityConfig в payment-service который пропускает все запросы на сервер

---
+++ payment-service/src/main/java/com/jonyshev/paymentservice/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.jonyshev.paymentservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityWebFilterChain chain(ServerHttpSecurity http) {
+        return http.csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(ex -> ex.anyExchange().permitAll())
+                .build();
+    }
+}
